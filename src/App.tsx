7b3b239f8cc84363a11498dd60dbe85c1d78468c
Thomas FORELL <thomas.forell@web.de>
2023-07-03 20:46:36 +0200

    Finally: controlled select with hooks

--- src/App.tsx
+++ src/App.tsx
@@ -25,8 +25,10 @@ import InputLabel from '@mui/material/InputLabel';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
+import Grid from '@mui/material/Unstable_Grid2';
 import { useForm } from 'react-hook-form';
+import { Control } from 'react-hook-form';
+import { Controller } from 'react-hook-form';
 
 const darkTheme = createTheme({
   palette: {
@@ -36,28 +38,35 @@ const darkTheme = createTheme({
 
 const variant = "standard";
 
-function TimeSelect(props: any)
+function TimeSelect(control: Control, name: string, label: string, value: number)
 {
   return (
     <FormControl sx={{ m: 1, minWidth: 120 }}>
-    <InputLabel>{props.label}</InputLabel>
-    <Select {...props}>
-        <MenuItem value="0s">0s</MenuItem>
-        <MenuItem value="5s">5s</MenuItem>
-        <MenuItem value="10s">10s</MenuItem>
-        <MenuItem value="20s">20s</MenuItem>
-        <MenuItem value="30s">30s</MenuItem>
-        <MenuItem value="45s">45s</MenuItem>
-        <MenuItem value="60s">60s</MenuItem>
-    </Select>
-  </FormControl>
+      <InputLabel>{label}</InputLabel>
+        <Controller control={control} name={name} defaultValue={value} render={({ field }) => (
+          <Select {...field} label={label} variant={variant}>
+            <MenuItem value={0}>0s</MenuItem>
+            <MenuItem value={5}>5s</MenuItem>
+            <MenuItem value={10}>10s</MenuItem>
+            <MenuItem value={20}>20s</MenuItem>
+            <MenuItem value={30}>30s</MenuItem>
+            <MenuItem value={45}>45s</MenuItem>
+            <MenuItem value={60}>60s</MenuItem>
+          </Select>)}/>
+    </FormControl>
   );
+//  return (
+//
+//  );
 }
 
 interface ConfigValues {
+  setup: {
+    output_name: Array<string>;
+  };
   team: {
     name: Array<string>;
-    time_values: Array<Array<string>>;
+    time_values: Array<Array<number>>;
     logo_home: Array<string>;
     logo_guest: Array<string>;
     num_players: Array<string>;
@@ -67,27 +76,40 @@ interface ConfigValues {
   };
   adv: {
     name: Array<string>;
-    time_values: Array<Array<string>>;
+    time_values: Array<Array<number>>;
   };
 };
 
-function TeamSettings(register: any, count: number) {
+function SetupSettings(register: any, count: number) {
+  return (
+      <Stack spacing={4} direction="row" alignItems="center">
+        <TextField label="Ausgabe Name" variant={variant} defaultValue="TV oder Stream" {...register("setup.output_name." + count.toString())}/>
+        <ButtonGroup variant="outlined" size="small" aria-label="">
+          <Button><AddIcon/></Button>
+          <Button><DeleteForeverIcon/></Button>
+          <Button><ArrowCircleUpIcon/></Button>
+          <Button><ArrowCircleDownIcon/></Button>
+        </ButtonGroup>
+      </Stack>
+  )
+}
+
+function TeamSettings(register: any, control: any, count: number) {
   let logoHome = "10";
   let logoGuest = "20";
-  
-   return (
-  
+
+  // <TimeSelect label="Zeit Livestream" defaultValue="0s" variant={variant} control={control} {...register("team.time_values." + count.toString() + ".0")}/>
+  // <TimeSelect label="Zeit TV Links" defaultValue="5s" variant={variant} control={control} {...register("team.time_values." + count.toString() + ".1")}/>
+  // <TimeSelect label="Zeit TV Rechts" defaultValue="10s" variant={variant} control={control} {...register("team.time_values." + count.toString() + ".2")}/>
+
+  return (
     <div>
       <Accordion>
         <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls=""
-          id="team_settings"> 
+          expandIcon={<ExpandMoreIcon />}> 
             <Stack spacing={4} direction="row" alignItems="center"  onClick={(event) => event.stopPropagation()}>
               <TextField id="team_name" label="Teamname" variant={variant} defaultValue="1. Mannschaft" {...register("team.name." + count.toString())}/>
-              <TimeSelect label="Zeit Livestream" defaultValue="0s" variant={variant} {...register("team.time_values." + count.toString() + ".0")}/>
-              <TimeSelect label="Zeit TV Links" defaultValue="5s" variant={variant} {...register("team.time_values." + count.toString() + ".1")}/>
-              <TimeSelect label="Zeit TV Rechts" defaultValue="10s" variant={variant} {...register("team.time_values." + count.toString() + ".2")}/>
+              {TimeSelect(control, "team.time_values." + count.toString() + ".0", "Zeit Livestream", 0)}
               <ButtonGroup variant="outlined" size="small" aria-label="">
                 <Button><AddIcon/></Button>
                 <Button><DeleteForeverIcon/></Button>
@@ -99,8 +121,8 @@ function TeamSettings(register: any, count: number) {
         <AccordionDetails>
           <Stack spacing={2} direction="column">
             <Grid container spacing={2}>
-              <Grid item xs={4}>
-                <FormControl style={{minWidth: 200}}>
+              <Grid xs={2}>
+                <FormControl>
                   <InputLabel id="logo_home_label">Logo Heim</InputLabel>
                   <Select labelId="logo_home_select_label"
                     id="logo_home_select"
@@ -114,11 +136,11 @@ function TeamSettings(register: any, count: number) {
                     </Select>
                 </FormControl>
               </Grid>
-              <Grid item xs={8}>
+              <Grid xs={10}>
                 <Box sx={{ height: 120, width: 250}}> <img src="SKC_Nibelungen_Lorsch.png" alt="" height="120" width="auto"/> </Box>
               </Grid>
-              <Grid item xs={4}>
-                <FormControl style={{minWidth: 200}}>
+              <Grid xs={2}>
+                <FormControl>
                   <InputLabel id="logo_guest_label">Logo Gast</InputLabel>
                   <Select labelId="logo_home_select_label"
                     id="logo_guest_select"
@@ -132,18 +154,16 @@ function TeamSettings(register: any, count: number) {
                   </Select>
                 </FormControl>
               </Grid>
-              <Grid item xs={8}>
+              <Grid xs={10}>
                 <Box sx={{ height: 120}}> <img src="KSC_Groß-Zimmern.jpg" alt="" height="120" width="auto"/> </Box>
               </Grid>
             </Grid>
-
           <Stack spacing={4} direction="row">
             <FormControl>
               <FormLabel id="num_player_label">Anzahl Spieler</FormLabel>
               <RadioGroup
                 row
-                aria-labelledby="num_player_label_aria"
-              >
+                aria-labelledby="num_player_label_aria">
                 <FormControlLabel value="4" control={<Radio />} label="4" {...register("team.num_players." + count.toString())}/>
                 <FormControlLabel value="6" control={<Radio />} label="6" {...register("team.num_players." + count.toString())}/>
               </RadioGroup>
@@ -152,8 +172,7 @@ function TeamSettings(register: any, count: number) {
               <FormLabel id="num_lanes_label">Anzahl Bahnen</FormLabel>
               <RadioGroup
                 row
-                aria-labelledby="num_lanes_label_aria"
-              >
+                aria-labelledby="num_lanes_label_aria">
                 <FormControlLabel value="4" control={<Radio />} label="4" {...register("team.num_lanes." + count.toString())}/>
                 <FormControlLabel value="6" control={<Radio />} label="6" {...register("team.num_lanes." + count.toString())}/>
               </RadioGroup>
@@ -169,6 +188,9 @@ function TeamSettings(register: any, count: number) {
 }
 
 function AdvSettings(register: any, count: number){
+//  <TimeSelect label="Zeit Livestream" defaultValue="0s" variant={variant} {...register("adv.time_values." + count.toString() + ".0")}/>
+//  <TimeSelect label="Zeit TV Links" defaultValue="5s" variant={variant} {...register("adv.time_values." + count.toString() + ".1")}/>
+//  <TimeSelect label="Zeit TV Rechts" defaultValue="10s" variant={variant} {...register("adv.time_values." + count.toString() + ".2")}/>
 
   return (
     <div>
@@ -179,9 +201,6 @@ function AdvSettings(register: any, count: number){
           id="panel1a-header"> 
             <Stack spacing={4} direction="row" alignItems="center" onClick={(event) => event.stopPropagation()}>
               <TextField id="standard-basic" label="Werbung" variant={variant} defaultValue="Kempa" {...register("adv.name." + count.toString())}/>
-                <TimeSelect label="Zeit Livestream" defaultValue="0s" variant={variant} {...register("adv.time_values." + count.toString() + ".0")}/>
-                <TimeSelect label="Zeit TV Links" defaultValue="5s" variant={variant} {...register("adv.time_values." + count.toString() + ".1")}/>
-                <TimeSelect label="Zeit TV Rechts" defaultValue="10s" variant={variant} {...register("adv.time_values." + count.toString() + ".2")}/>
                 <ButtonGroup size="small" variant="outlined" aria-label="outlined button group">
                 <Button onClick={() => {}} ><AddIcon/></Button>
                 <Button><DeleteForeverIcon/></Button>
@@ -193,7 +212,7 @@ function AdvSettings(register: any, count: number){
         <AccordionDetails>
           <Stack spacing={2} direction="column">
             <Grid container spacing={2}>
-              <Grid item xs={4}>
+              <Grid xs={4}>
                 <FormControl style={{minWidth: 200}}>
                   <InputLabel id="demo-simple-select-label">Werbung</InputLabel>
                   <Select labelId="demo-simple-select-label"
@@ -206,11 +225,10 @@ function AdvSettings(register: any, count: number){
                     </Select>
                 </FormControl>
               </Grid>
-              <Grid item xs={8}>
+              <Grid xs={8}>
                 <Box sx={{ height: 120, width: 250}}> <img  src="rekorde_einzel_120.png" alt="" height="120" width="auto"/> </Box>
               </Grid>
             </Grid>
-
         </Stack>
         </AccordionDetails>
       </Accordion>
@@ -219,25 +237,52 @@ function AdvSettings(register: any, count: number){
 }
 
 function App() {
-  
-  const { register, watch } = useForm<ConfigValues>();
+
+  const { control, register, watch, setValue } = useForm<ConfigValues>();
   let watchedValues = watch();
 
   return (
     <>
       <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-      <h1>Team Konfiguration</h1>
-      <p>
-        {TeamSettings(register, 0)}
-        {TeamSettings(register, 1)}
-        <Button onClick={() => {console.log(watchedValues.team);}} variant="contained">Speichern</Button>
-      </p>
-      <h1>Werbung Konfiguration</h1>
-      <p>
-        {AdvSettings(register, 0)}
-        <Button onClick={() => {console.log(watchedValues.adv);}} variant="contained">Speichern</Button>
-      </p>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Stack spacing={2} direction="row" alignItems="center"  onClick={(event) => event.stopPropagation()}>
+            <h1>Setup</h1>
+            <Button onClick={() => {console.log(watchedValues.setup);}} variant="contained">Speichern</Button>
+          </Stack>
+        </AccordionSummary>
+        <AccordionDetails>
+          {SetupSettings(register, 0)}
+          {SetupSettings(register, 1)}
+          {SetupSettings(register, 2)}
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Stack spacing={2} direction="row" alignItems="center"  onClick={(event) => event.stopPropagation()}>
+            <h1>Team Konfiguration</h1>
+            <Button onClick={() => {console.log(watchedValues.team);}} variant="contained">Speichern</Button>
+          </Stack>
+        </AccordionSummary>
+        <AccordionDetails>
+          {TeamSettings(register, control, 0)}
+          {TeamSettings(register, control, 1)}
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Stack spacing={2} direction="row" alignItems="center"  onClick={(event) => event.stopPropagation()}>
+            <h1>Werbung Konfiguration</h1>
+            <Button onClick={() => {console.log(watchedValues.adv);}} variant="contained">Speichern</Button>
+          </Stack>
+        </AccordionSummary>
+        <AccordionDetails>
+          {AdvSettings(register, 0)}
+        </AccordionDetails>
+      </Accordion>
+      <Button onClick={() => { setValue("setup.output_name.0", "Livestream"); setValue("setup.output_name.1", "TV Links"); 
+        setValue("setup.output_name.2", "TV Rechts"); setValue("team.time_values.0.0", 60)}}>Test Values</Button>
       </ThemeProvider>
     </>
   );
