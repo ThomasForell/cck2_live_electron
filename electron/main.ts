e93cb3afbb4cd0d894b81ce6419f96c8deddcb85
Thomas FORELL <thomas.forell@web.de>
2023-08-29 01:08:14 +0200

    Start html response.

--- electron/main.ts
+++ electron/main.ts
@@ -14,6 +14,7 @@ const indexUrls = ["/", "/index.html"]
 const displayUrls = ["/TVLinks.html", "/TVRechts.html"];
 const streamUrls = ["/Stream.html"];
 const configUrls = ["/TVLinks.json", "/TVRechts.json", "/Stream.json"];
+const constPixUrls = ["/Wurf_Gesamt.png", "/Holz_Gesamt.png", "/Wurf_Durchgang.png", "/Holz_Durchgang.png"];
 
 let configValues: ConfigValues;
 
@@ -80,6 +81,9 @@ express_app.use( (req, res, next) => {
   else if (configUrls.indexOf(req.originalUrl) >= 0) {
     const id = configUrls.indexOf(req.originalUrl);
     res.json(createConfig(id));
+  }
+  else if (constPixUrls.indexOf(req.originalUrl) >= 0) {
+    res.sendFile(path.resolve(`${__dirname}/../../static-html` + req.originalUrl));
   }
   else {
     next();
