36595d34b9ff9dc6924d0d05085d567d1220876d
Thomas FORELL <thomas.forell@web.de>
2023-10-24 02:01:30 +0200

    Format files.

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,8 @@ import Tab from '@mui/material/Tab';
 
 import { useForm } from 'react-hook-form';
 
-import {ConfigValues} from '../cck2_live_interface/ConfigValues';
-import TabLeague from'./TabLeague';
+import { ConfigValues } from '../cck2_live_interface/ConfigValues';
+import TabLeague from './TabLeague';
 import TabSingle from './TabSingle';
 import TabSprint from './TabSprint';
 import TabTeam from './TabTeam';
@@ -22,119 +22,121 @@ import TabInfo from './TabInfo';
 
 
 interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
+    children?: React.ReactNode;
+    index: number;
+    value: number;
 }
 
 function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
+    const { children, value, index, ...other } = props;
 
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`vertical-tabpanel-${index}`}
-      aria-labelledby={`vertical-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          {children}
-        </Box>
-      )}
-    </div>
-  );
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`vertical-tabpanel-${index}`}
+            aria-labelledby={`vertical-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    {children}
+                </Box>
+            )}
+        </div>
+    );
 }
 
 
 const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
+    palette: {
+        mode: 'dark',
+    },
 });
 
 export const variant = "standard";
-export const controlFktContext = createContext((null as any) as {watchedValues: ConfigValues, setStateUpdate: Function, setValue: Function} );
+export const controlFktContext = createContext((null as any) as { watchedValues: ConfigValues, setStateUpdate: Function, setValue: Function });
 
 function App() {
-  let currentVersion = "";
+    let currentVersion = "";
 
-  function setCurrentVersion(version: string) {
-    currentVersion = version;
-  }
+    function setCurrentVersion(version: string) {
+        currentVersion = version;
+    }
 
-  const { control, register, watch, setValue } = useForm<ConfigValues>();
-  let watchedValues: ConfigValues = watch();
-  let stateUpdate: ConfigValues;
-  let setStateUpdate: Function;
-  [stateUpdate, setStateUpdate] = useState(watchedValues);
+    const { control, register, watch, setValue } = useForm<ConfigValues>();
+    let watchedValues: ConfigValues = watch();
+    let stateUpdate: ConfigValues;
+    let setStateUpdate: Function;
+    [stateUpdate, setStateUpdate] = useState(watchedValues);
 
-  useEffect(() => {
-    setValue("setup", stateUpdate.setup);
-    setValue("team", stateUpdate.team);
-    setValue("adv", stateUpdate.adv);
-  }, [stateUpdate]);
+    useEffect(() => {
+        setValue("setup", stateUpdate.setup);
+        setValue("team", stateUpdate.team);
+        setValue("adv", stateUpdate.adv);
+    }, [stateUpdate]);
 
-  const [mainValuePanel, setMainValuePanel] = useState(0);
-  const handleChangeMainMenu = (event: React.SyntheticEvent, newValue: number) => {
-    setMainValuePanel(newValue);
-  };
+    const [mainValuePanel, setMainValuePanel] = useState(0);
+    const handleChangeMainMenu = (event: React.SyntheticEvent, newValue: number) => {
+        setMainValuePanel(newValue);
+    };
 
-  const [activeOutput, setActiveOutput] = useState("liga");
+    const [activeOutput, setActiveOutput] = useState("liga");
 
-  const values: ConfigValues = {...watchedValues};
-  const dataStuff = {watchedValues: watchedValues, 
-    setStateUpdate: setStateUpdate, 
-    setValue: setValue
-  }; 
+    const values: ConfigValues = { ...watchedValues };
+    const dataStuff = {
+        watchedValues: watchedValues,
+        setStateUpdate: setStateUpdate,
+        setValue: setValue
+    };
 
-  useEffect(
-    () => {
-      (window as any).electronAPI.load().then(({config: data, version: version}: {config: ConfigValues, version: string}) => {
-        setStateUpdate(data);
-        setCurrentVersion(version);
-        setActiveOutput(data.setup.active_output);
-        console.log("load return");
-      }); 
-      return () => { };}, [] );
+    useEffect(
+        () => {
+            (window as any).electronAPI.load().then(({ config: data, version: version }: { config: ConfigValues, version: string }) => {
+                setStateUpdate(data);
+                setCurrentVersion(version);
+                setActiveOutput(data.setup.active_output);
+                console.log("load return");
+            });
+            return () => { };
+        }, []);
 
-  return (
-    <controlFktContext.Provider value={dataStuff}>
-      <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={mainValuePanel} onChange={handleChangeMainMenu} aria-label="basic tabs example">
-          <Tab icon={activeOutput === "league" ? <WiFi/> : <SignalWifiStatusbarNullIcon/>} iconPosition="start" label="Liga" id="main-panel-liga"/>
-          <Tab icon={activeOutput === "single" ? <WiFi/> : <SignalWifiStatusbarNullIcon/>} iconPosition="start" label="Einzel" id="main-panel-single"/>
-          <Tab icon={activeOutput === "sprint" ? <WiFi/> : <SignalWifiStatusbarNullIcon/>} iconPosition="start" label="Sprint" id="main-panel-spirnt"/>
-          <Tab icon={activeOutput === "team" ? <WiFi/> : <SignalWifiStatusbarNullIcon/>} iconPosition="start" label="Team" id="main-panel-team"/>
-          <Tab label="Setup" id="main-panel-2"/>
-          <Tab label="Info" id="main-panel-3"/>
-        </Tabs>
-      </Box>
-      <TabPanel value={mainValuePanel} index={0}>
-        <TabLeague register={register} control={control} watchedValues={watchedValues}/>
-      </TabPanel>
-      <TabPanel value={mainValuePanel} index={1}>
-        <TabSingle/>
-      </TabPanel>
-      <TabPanel value={mainValuePanel} index={2}>
-        <TabSprint/>
-      </TabPanel>
-      <TabPanel value={mainValuePanel} index={3}>
-        <TabTeam/>
-      </TabPanel>
-      <TabPanel value={mainValuePanel} index={4}>
-        <TabSetup register={register} control={control} settings={values.setup} watchedValues={watchedValues} setActiveOutput={setActiveOutput}/>
-      </TabPanel>
-      <TabPanel value={mainValuePanel} index={5}>
-        <TabInfo version={currentVersion}/>
-      </TabPanel>
+    return (
+        <controlFktContext.Provider value={dataStuff}>
+            <ThemeProvider theme={darkTheme}>
+                <CssBaseline />
+                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                    <Tabs value={mainValuePanel} onChange={handleChangeMainMenu} aria-label="basic tabs example">
+                        <Tab icon={activeOutput === "league" ? <WiFi /> : <SignalWifiStatusbarNullIcon />} iconPosition="start" label="Liga" id="main-panel-liga" />
+                        <Tab icon={activeOutput === "single" ? <WiFi /> : <SignalWifiStatusbarNullIcon />} iconPosition="start" label="Einzel" id="main-panel-single" />
+                        <Tab icon={activeOutput === "sprint" ? <WiFi /> : <SignalWifiStatusbarNullIcon />} iconPosition="start" label="Sprint" id="main-panel-spirnt" />
+                        <Tab icon={activeOutput === "team" ? <WiFi /> : <SignalWifiStatusbarNullIcon />} iconPosition="start" label="Team" id="main-panel-team" />
+                        <Tab label="Setup" id="main-panel-2" />
+                        <Tab label="Info" id="main-panel-3" />
+                    </Tabs>
+                </Box>
+                <TabPanel value={mainValuePanel} index={0}>
+                    <TabLeague register={register} control={control} watchedValues={watchedValues} />
+                </TabPanel>
+                <TabPanel value={mainValuePanel} index={1}>
+                    <TabSingle />
+                </TabPanel>
+                <TabPanel value={mainValuePanel} index={2}>
+                    <TabSprint />
+                </TabPanel>
+                <TabPanel value={mainValuePanel} index={3}>
+                    <TabTeam />
+                </TabPanel>
+                <TabPanel value={mainValuePanel} index={4}>
+                    <TabSetup register={register} control={control} settings={values.setup} watchedValues={watchedValues} setActiveOutput={setActiveOutput} />
+                </TabPanel>
+                <TabPanel value={mainValuePanel} index={5}>
+                    <TabInfo version={currentVersion} />
+                </TabPanel>
 
-      </ThemeProvider>
-    </controlFktContext.Provider>
-  );
+            </ThemeProvider>
+        </controlFktContext.Provider>
+    );
 }
 
 export default App;
--- src/LogoDropzone.tsx
+++ src/LogoDropzone.tsx
@@ -8,57 +8,57 @@ import { Controller } from "react-hook-form";
 
 
 function LogoDropzone({ label, name, value, control }: { label: string; name: string; value: string; control: Control; }) {
-  let source = "";
-  if (name.startsWith("adv")) {
-    source = "http://localhost/logos/adv/" + value;
-  }
-  else {
-    source = "http://localhost/logos/team/" + value;
-  }
-  return (
-    <Stack spacing={2} direction="row" alignItems="center">
-      <Box sx={{ height: 120, width: 120 }}>
-        <div style={{ textAlign: "start" }}>
-          {label}
-        </div>
-      </Box>
-      <Box sx={{ height: 120, width: 250 }}>
-        <div style={{ textAlign: "center" }}>
-          <img id={name} src={source} alt=" Warte auf Logo" height="120" width="auto" />
-        </div>
-      </Box>
-      <Controller control={control} name={name}
-        render={({ field: { onChange, value } }) => <Dropzone
-          noClick noKeyboard
-          accept={{ 'image/*': ['.jpeg', '.png'] }}
-          multiple={false}
-          onDrop={(acceptedFiles: File[]) => {
-            if (name.startsWith("team")) {
-              (window as any).electronAPI.logo("team", acceptedFiles[0].name, (acceptedFiles[0] as any).path).then(
-                (filename: string | null) => { if (filename != null) { onChange(filename); } });
-            }
-            else {
-              (window as any).electronAPI.logo("adv", acceptedFiles[0].name, (acceptedFiles[0] as any).path).then(
-                (filename: string | null) => { if (filename != null) { onChange(filename); } });
-            }
-          } }>
-          {({ getRootProps, getInputProps, open, isDragReject, isDragActive, isDragAccept }) => (
-            <Box sx={{
-              height: 120, width: 250, borderRadius: 2, border: "2px dashed",
-              ...(isDragReject && { background: "#460100" }), ...(isDragAccept && { background: "#092005" })
-            }}>
-              <div {...getRootProps()} style={{ textAlign: "center" }}>
-                <input {...getInputProps()} />
-                {!isDragActive && (<p>Logo in diesen Bereich ziehen</p>)}
-                {isDragAccept && (<p>Logo auswählen</p>)}
-                {isDragReject && (<p>Logo muss eine Bilddatei sein</p>)}
-                <Button onClick={open} variant="contained">Logo Auswählen</Button>
-              </div>
+    let source = "";
+    if (name.startsWith("adv")) {
+        source = "http://localhost/logos/adv/" + value;
+    }
+    else {
+        source = "http://localhost/logos/team/" + value;
+    }
+    return (
+        <Stack spacing={2} direction="row" alignItems="center">
+            <Box sx={{ height: 120, width: 120 }}>
+                <div style={{ textAlign: "start" }}>
+                    {label}
+                </div>
             </Box>
-          )}
-        </Dropzone>} />
-    </Stack>
-  );
+            <Box sx={{ height: 120, width: 250 }}>
+                <div style={{ textAlign: "center" }}>
+                    <img id={name} src={source} alt=" Warte auf Logo" height="120" width="auto" />
+                </div>
+            </Box>
+            <Controller control={control} name={name}
+                render={({ field: { onChange, value } }) => <Dropzone
+                    noClick noKeyboard
+                    accept={{ 'image/*': ['.jpeg', '.png'] }}
+                    multiple={false}
+                    onDrop={(acceptedFiles: File[]) => {
+                        if (name.startsWith("team")) {
+                            (window as any).electronAPI.logo("team", acceptedFiles[0].name, (acceptedFiles[0] as any).path).then(
+                                (filename: string | null) => { if (filename != null) { onChange(filename); } });
+                        }
+                        else {
+                            (window as any).electronAPI.logo("adv", acceptedFiles[0].name, (acceptedFiles[0] as any).path).then(
+                                (filename: string | null) => { if (filename != null) { onChange(filename); } });
+                        }
+                    }}>
+                    {({ getRootProps, getInputProps, open, isDragReject, isDragActive, isDragAccept }) => (
+                        <Box sx={{
+                            height: 120, width: 250, borderRadius: 2, border: "2px dashed",
+                            ...(isDragReject && { background: "#460100" }), ...(isDragAccept && { background: "#092005" })
+                        }}>
+                            <div {...getRootProps()} style={{ textAlign: "center" }}>
+                                <input {...getInputProps()} />
+                                {!isDragActive && (<p>Logo in diesen Bereich ziehen</p>)}
+                                {isDragAccept && (<p>Logo auswählen</p>)}
+                                {isDragReject && (<p>Logo muss eine Bilddatei sein</p>)}
+                                <Button onClick={open} variant="contained">Logo Auswählen</Button>
+                            </div>
+                        </Box>
+                    )}
+                </Dropzone>} />
+        </Stack>
+    );
 }
 
 export default LogoDropzone;
