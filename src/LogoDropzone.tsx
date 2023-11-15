26a81f15dba01599262dabd362d74d221ecd16da
Thomas FORELL <thomas.forell@web.de>
2023-11-16 00:18:16 +0100

    Add dense mode to LogoDropzone

--- src/LogoDropzone.tsx
+++ src/LogoDropzone.tsx
@@ -7,7 +7,7 @@ import { Control } from "react-hook-form";
 import { Controller } from "react-hook-form";
 
 
-function LogoDropzone({ label, name, value, control }: { label: string; name: string; value: string; control: Control; }) {
+function LogoDropzone({ label, name, value, control, dense=false }: { label: string; name: string; value: string; control: Control, dense?: boolean; }) {
     let source = "";
     if (name.startsWith("adv")) {
         source = "http://localhost/logos/adv/" + value;
@@ -15,16 +15,22 @@ function LogoDropzone({ label, name, value, control }: { label: string; name: st
     else {
         source = "http://localhost/logos/team/" + value;
     }
+    
+    let height = 120;
+    if (dense) {
+        height = 80;
+    }
+
     return (
         <Stack spacing={2} direction="row" alignItems="center">
-            <Box sx={{ height: 120, width: 120 }}>
+            <Box sx={{ height: {height}, width: 120 }}>
                 <div style={{ textAlign: "start" }}>
                     {label}
                 </div>
             </Box>
-            <Box sx={{ height: 120, width: 250 }}>
+            <Box sx={{ height: {height}, width: 250 }}>
                 <div style={{ textAlign: "center" }}>
-                    <img id={name} src={source} alt=" Warte auf Logo" height="120" width="auto" />
+                    <img id={name} src={source} alt=" Warte auf Logo" height={height} width="auto" />
                 </div>
             </Box>
             <Controller control={control} name={name}
@@ -44,15 +50,17 @@ function LogoDropzone({ label, name, value, control }: { label: string; name: st
                     }}>
                     {({ getRootProps, getInputProps, open, isDragReject, isDragActive, isDragAccept }) => (
                         <Box sx={{
-                            height: 120, width: 250, borderRadius: 2, border: "2px dashed",
+                            height: {height}, width: 250, borderRadius: 2, border: "2px dashed", padding: "8px",
                             ...(isDragReject && { background: "#460100" }), ...(isDragAccept && { background: "#092005" })
                         }}>
                             <div {...getRootProps()} style={{ textAlign: "center" }}>
                                 <input {...getInputProps()} />
-                                {!isDragActive && (<p>Logo in diesen Bereich ziehen</p>)}
-                                {isDragAccept && (<p>Logo auswählen</p>)}
-                                {isDragReject && (<p>Logo muss eine Bilddatei sein</p>)}
-                                <Button onClick={open} variant="contained">Logo Auswählen</Button>
+                                <Stack spacing={2} direction={dense ? "row" : "column"}>
+                                    <Button onClick={open} variant="contained">Logo Auswählen</Button>
+                                    {!isDragActive && (<p>Logo in diesen Bereich ziehen</p>)}
+                                    {isDragAccept && (<p>Logo auswählen</p>)}
+                                    {isDragReject && (<p>Logo muss eine Bilddatei sein</p>)}
+                                </Stack>
                             </div>
                         </Box>
                     )}
